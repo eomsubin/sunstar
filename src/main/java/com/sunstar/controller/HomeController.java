cb2146edd06acf2b9c129e63ef6181858651fa5f
jinwo <jinwo@LAPTOP-QIEPQOES>
2020-02-17 19:32:22 +0900

    root-context.xml 경로 수정 main과 Naverlogin 추가

--- src/main/java/com/sunstar/controller/HomeController.java
+++ src/main/java/com/sunstar/controller/HomeController.java
@@ -2,38 +2,87 @@ package com.sunstar.controller;
 
 import java.text.DateFormat;
 import java.util.Date;
+import java.util.HashMap;
 import java.util.Locale;
+
+import javax.servlet.http.HttpSession;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 /**
  * Handles requests for the application home page.
  */
 @Controller
 public class HomeController {
-	
+
 	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
-	
+
 	/**
 	 * Simply selects the home view to render by returning its name.
 	 */
-	@RequestMapping(value = "/", method = RequestMethod.GET)
-	public String home(Locale locale, Model model) {
-		logger.info("Welcome home! The client locale is {}.", locale);
-		
-		Date date = new Date();
-		DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
-		
-		String formattedDate = dateFormat.format(date);
-		
-		model.addAttribute("serverTime", formattedDate );
-		
+	@RequestMapping("/header")
+	public String header()
+	{
+		return "header";
+	}
+
+	@RequestMapping("/footer")
+	public String footer()
+	{
+		return "footer";
+	}
+
+	@RequestMapping(value = "/main/{page}", method = RequestMethod.GET)
+	public String home(@PathVariable String page, Model model) {
+		model.addAttribute("contentpage", page);
 		return "home";
 	}
+
+	@RequestMapping("/")
+	public String main(Model model)
+	{
+		model.addAttribute("contentpage", "body");
+		return "home";
+	}
+
+	@RequestMapping("/body")
+	public String body(Locale locale, Model model)
+	{
+		logger.info("Welcome home! The client locale is {}.", locale);
+
+		Date date = new Date();
+		DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
+
+		String formattedDate = dateFormat.format(date);
+		model.addAttribute("serverTime", formattedDate );		
+
+		return "body";
+	}
+
+	@RequestMapping("/login")
+	public String login()
+	{
+		return "login";
+	}
+
+	@RequestMapping("/callback")
+	public String callback(HttpSession session) {
+		return "callback";
+	}
 	
+	@ResponseBody
+	@RequestMapping("/loginNaver")
+	public void loginNaver(HashMap<String, String> user, String age){
+		System.out.println(user);
+		System.out.println(age);
+		System.out.println("¹Ùº¸");
+	}
 }
--- src/main/webapp/WEB-INF/views/home.jsp
+++ src/main/webapp/WEB-INF/views/home.jsp
@@ -1,14 +1,42 @@
-<%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c" %>
-<%@ page session="false" %>
+<%@ page language="java" contentType="text/html; charset=utf-8"
+	pageEncoding="utf-8"%>
+<%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c"%>
+<%@ page session="false"%>
+<!DOCTYPE html>
 <html>
 <head>
-	<title>Home</title>
+<!-- Required meta tags -->
+<meta charset="utf-8">
+<meta name="viewport"
+	content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+<!-- Bootstrap CSS -->
+<link rel="stylesheet"
+	href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css"
+	integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh"
+	crossorigin="anonymous">
+
+<script src="https://code.jquery.com/jquery-3.4.1.min.js"></script>
+<title>SBBJ</title>
 </head>
 <body>
-<h1>
-	Hello world!  
-</h1>
+	<header>
+		<c:import url="http://localhost:8080/controller/header" />
+	</header>
+	<c:import url="http://localhost:8080/controller/${contentpage}" />
+	<footer>
+		<c:import url="http://localhost:8080/controller/footer" />
+	</footer>
 
-<P>  The time on the server is ${serverTime}. </P>
+	<!-- Popper.js first, then Bootstrap JS -->
+	<script
+		src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js"
+		integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo"
+		crossorigin="anonymous"></script>
+	<script
+		src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"
+		integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6"
+		crossorigin="anonymous"></script>
 </body>
 </html>
+
