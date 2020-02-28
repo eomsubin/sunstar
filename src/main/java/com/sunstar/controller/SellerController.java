eb50e5d443ddff4e980d4ff234639fee0e0824a7
solyi <choi-solyi@naver.com>
2020-02-28 14:33:49 +0900

    SELLER 업데이트

--- src/main/java/com/sunstar/controller/SellerController.java
+++ src/main/java/com/sunstar/controller/SellerController.java
@@ -7,9 +7,11 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -38,9 +40,19 @@ public class SellerController {
 		List<ProductDTO> plist = sellerservice.list();		
 		
 		model.addAttribute("plist", plist);
-		System.out.println("--");
-		System.out.println(plist.get(0).getOptions());
-		System.out.println("--");
+		
+		//
+		//페이지에 카테고리 표시
+		List<CategoryDTO> clist = sellerservice.getCategory();
+		List<CategoryDTO> dlist = new ArrayList<>();
+		for(CategoryDTO dto : clist) {
+			dto.setLv123(dto.getLv1()+" - "+dto.getLv2()+" - "+dto.getLv3()); 
+			dlist.add(dto);
+		}
+		model.addAttribute("dlist", dlist);
+		//
+		
+		
 		model.addAttribute("sellerpage", "productlist.jsp");
 		return "sellers/temp";
 	}
@@ -64,10 +76,81 @@ public class SellerController {
 	
 	@RequestMapping("/addproductresult")
 	public String addproductresult(ProductDTO dto) {
-
-		
 		sellerservice.addProduct(dto);
+		return "redirect:/productlist";
+	}
+	
+	@RequestMapping("/deleteproduct/{pcode}")
+	public String deleteproduct(@PathVariable int pcode ) {
 		
+		sellerservice.deleteProduct(pcode);
+		return "redirect:/productlist";
+	}
+	
+	private List<Integer> plist = new ArrayList<>();
+
+	@RequestMapping("/updateinventory/{pcode}/{gesu}")
+	public String updateinventory(@PathVariable int pcode, @PathVariable int gesu){
+
+		Object a = (Object)pcode;
+		
+		if(!plist.contains(a)) {
+			plist.add(pcode);
+			System.out.println("성공" + pcode);
+		}else {
+			System.out.println("실패" + pcode);
+		}
+		
+		
+		System.out.println("size: "+plist.size());
+		
+		for(int i=0;i<plist.size();i++) {
+			System.out.println("index  -------- "+i);
+			System.out.println("value ----------"+plist.get(i));
+		}
+
+		/*System.out.println("////");
+		System.out.println(plist);
+		System.out.println("////");
+		for(int i=0;i<plist.size();i++) {
+
+			int product = plist.get(i);		
+			List<String> color = sellerservice.getOptioncolor(product);
+			
+			for(String aaa : color) {
+				System.out.println("--------------시작--");
+				System.out.println(pcode);
+				System.out.println(aaa);
+				System.out.println("---------------끝-");
+				OptionDTO dto = new OptionDTO();
+				dto.setProduct_code(pcode);
+				dto.setInventory(gesu);
+				dto.setColor(aaa);
+				sellerservice.updateinventory(dto);
+			}
+		}*/
+		
+		//	System.out.println("-----------pcode---시작--");
+	//	System.out.println(pcode);
+	//	System.out.println("-----------pcode----끝-");
+		
+		
+	//	List<String> color = sellerservice.getOptioncolor(pcode);
+	
+	//	System.out.println("color 출력" + color);	*/
+	/*	for(String a : color) {
+			System.out.println("--------------시작--");
+
+			System.out.println(pcode);
+			System.out.println(a);
+			System.out.println("---------------끝-");
+			OptionDTO dto = new OptionDTO();
+			dto.setProduct_code(pcode);
+			dto.setInventory(gesu);
+			dto.setColor(a);
+			sellerservice.updateinventory(dto);
+		}*/
+
 		return "redirect:/productlist";
 	}
 }
